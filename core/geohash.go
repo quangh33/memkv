9b6fbd81a38ff8dc3b169511d1710b649b891e75
quangh33 <quanghd.95vn@gmail.com>
2023-08-26 22:42:00 +0800

    geohash: clean up

--- core/geohash.go
+++ core/geohash.go
@@ -33,9 +33,9 @@ type GeohashRange struct {
 /*
 break x into 5-bit blocks and map each block to a character in GeoAlphabet.
 If x is 52-bit long, the 2 last bits are encoded as 0. Example:
-0b10010 11010 10010 10110 10100 10101 10101 00101 01101 01001 01
 
-	v     u     q     q     q     p     p     5     e     9     0
+	  0b10010 11010 10010 10110 10100 10101 10101 00101 01101 01001 01
+		v     u     q     q     q     p     p     5     e     9     0
 */
 func Base32Encode(x uint64) string {
 	b := [11]byte{}

d23ecd7d0ed3f22c177825d6fc528b6c9f48b437
quangh33 <quanghd.95vn@gmail.com>
2023-08-27 23:18:26 +0800

    use testify for testing

--- internal/data_structure/skiplist_test.go
+++ internal/data_structure/skiplist_test.go
@@ -1,26 +1,17 @@
 package data_structure
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestCreateSkiplist(t *testing.T) {
 	sl := CreateSkiplist()
-	if sl == nil {
-		t.Fail()
-	}
-	if sl.length != 0 {
-		t.Fail()
-	}
-	if sl.level != 1 {
-		t.Fail()
-	}
-	if len(sl.head.levels) != SkiplistMaxLevel {
-		t.Fail()
-	}
-	if sl.tail != nil {
-		t.Fail()
-	}
+	assert.NotNil(t, sl)
+	assert.EqualValues(t, 0, sl.length)
+	assert.EqualValues(t, 1, sl.level)
+	assert.EqualValues(t, SkiplistMaxLevel, len(sl.head.levels))
+	assert.Nil(t, sl.tail)
 }
 
 func TestSkiplist_Insert(t *testing.T) {
@@ -29,39 +20,18 @@ func TestSkiplist_Insert(t *testing.T) {
 	sl.Insert(20, "k3")
 	sl.Insert(40, "k4")
 	sl.Insert(10, "k2")
-	if sl.length != 4 {
-		t.Fatal("len should be 4")
-	}
-	if sl.head.levels[0].forward.score != 10 {
-		t.Fatal("1st node score should be 10")
-	}
-	if sl.head.levels[0].forward.ele != "k1" {
-		t.Fatal("1st node should be k1")
-	}
-	if sl.head.levels[0].forward.levels[0].forward.score != 10 {
-		t.Fatal("2nd node score should be 10")
-	}
-	if sl.head.levels[0].forward.levels[0].forward.ele != "k2" {
-		t.Fatal("2nd node should be k2")
-	}
-	if sl.head.levels[0].forward.levels[0].forward.levels[0].forward.score != 20 {
-		t.Fatal("3rd node score should be 20")
-	}
-	if sl.head.levels[0].forward.levels[0].forward.levels[0].forward.ele != "k3" {
-		t.Fatal("3rd node score should be k3")
-	}
-	if sl.head.levels[0].forward.backward != nil {
-		t.Fail()
-	}
-	if sl.tail != sl.head.levels[0].forward.levels[0].forward.levels[0].forward.levels[0].forward {
-		t.Fatal("tail should be 4th node")
-	}
-	if sl.head.levels[0].span != 1 {
-		t.Fail()
-	}
+
+	assert.EqualValues(t, 4, sl.length)
+	assert.EqualValues(t, 10, sl.head.levels[0].forward.score)
+	assert.EqualValues(t, "k1", sl.head.levels[0].forward.ele)
+	assert.EqualValues(t, 10, sl.head.levels[0].forward.levels[0].forward.score)
+	assert.EqualValues(t, "k2", sl.head.levels[0].forward.levels[0].forward.ele)
+	assert.EqualValues(t, 20, sl.head.levels[0].forward.levels[0].forward.levels[0].forward.score)
+	assert.EqualValues(t, "k3", sl.head.levels[0].forward.levels[0].forward.levels[0].forward.ele)
+	assert.Nil(t, sl.head.levels[0].forward.backward)
+	assert.Equal(t, sl.head.levels[0].forward.levels[0].forward.levels[0].forward.levels[0].forward, sl.tail)
+	assert.EqualValues(t, 1, sl.head.levels[0].span)
 	for i := sl.level; i < SkiplistMaxLevel; i++ {
-		if sl.head.levels[i].span != 0 {
-			t.Fail()
-		}
+		assert.EqualValues(t, 0, sl.head.levels[i].span)
 	}
 }

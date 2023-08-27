1d68eded24798de9c2d82fd309cf65f05e497a41
quangh33 <quanghd.95vn@gmail.com>
2023-08-27 16:47:40 +0800

    skiplist: cleanup

--- core/skiplist.go
+++ core/skiplist.go
@@ -12,8 +12,8 @@ const SkiplistMaxLevel = 32
 	|level 2: span=2 | forward| --------------------------------------> |span=0 | forward| ----> NULL
 	|level 1: span=1 | forward| --------> |span=1 | forward| ---------> |span=0 | forward| ----> NULL
 	|ele					  |			  |ele			   |			|ele			 |
-	|score					  |			  |score		   |			|score           |
-    |backward				  | <-------- |backward        | <--------- |backward		 |
+	|score					  |			  |score		   |			|score			 |
+	|backward				  | <-------- |backward		   | <--------- |backward		 |
 	|node1					  |			  |node2		   |			|node3			 |
 */
 

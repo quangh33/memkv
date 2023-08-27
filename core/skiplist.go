048839a70a4cfe6ad2f398ff545a1cde5abe77bc
quangh33 <quanghd.95vn@gmail.com>
2023-08-27 16:52:26 +0800

    skiplist: cleanup

--- core/skiplist.go
+++ core/skiplist.go
@@ -9,12 +9,12 @@ const SkiplistMaxLevel = 32
 
 /*
 
-	|level 2: span=2 | forward| --------------------------------------> |span=0 | forward| ----> NULL
-	|level 1: span=1 | forward| --------> |span=1 | forward| ---------> |span=0 | forward| ----> NULL
+	/level 2: span=2 | forward\ --------------------------------------> /span=0 | forward\ ----> NULL
+	|level 1: span=1 | forward| --------> /span=1 | forward\ ---------> |span=0 | forward| ----> NULL
 	|ele                      |           |ele             |            |ele             |
 	|score                    |           |score           |            |score           |
 	|backward                 | <-------- |backward        | <--------- |backward        |
-	|node1                    |           |node2           |            |node3           |
+	\node1                    /           \node2           /            \node3           /
 */
 
 type SkiplistLevel struct {

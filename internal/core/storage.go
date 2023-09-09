3863534bbdfcdfabe481bd310a76e3d88ba79978
quangh33 <quanghd.95vn@gmail.com>
2023-09-09 18:25:07 +0800

    Refactor

--- internal/core/storage.go
+++ internal/core/storage.go
@@ -4,21 +4,12 @@ import (
 	"memkv/internal/data_structure"
 )
 
-type Obj struct {
-	Value        interface{}
-	TypeEncoding uint8
-	// type    | encoding
-	// [][][][]|[][][][]
-}
-
 var zsetStore map[string]*data_structure.ZSet
-
 var setStore map[string]data_structure.Set
-
-var dict *data_structure.Dict
+var dictStore *data_structure.Dict
 
 func init() {
 	zsetStore = make(map[string]*data_structure.ZSet)
 	setStore = make(map[string]data_structure.Set)
-	dict = data_structure.CreateDict()
+	dictStore = data_structure.CreateDict()
 }
